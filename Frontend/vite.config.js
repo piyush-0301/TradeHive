40827faadec66f72585611272455c7e449606f20
piyush-0301 <piyushudapurkar12@gmail.com>
2026-04-25 04:13:06 +0530

    Fix white screen: update output directory and base path

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react' // <--- Make sure it says this!
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // This tells the browser to look in the current folder for files
 })
