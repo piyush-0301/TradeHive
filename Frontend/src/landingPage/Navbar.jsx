c304a28d8a3d0023cbdf9e810c6424d4f91e7285
piyush-0301 <piyushudapurkar12@gmail.com>
2026-04-25 05:06:13 +0530

    Fix: Switched to Bootstrap CDN to solve build error

--- Frontend/src/landingPage/Navbar.jsx
+++ Frontend/src/landingPage/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom'
 
 const NavbarCont = () => {
